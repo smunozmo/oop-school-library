5539977390f4d93d33435aae1b27202b976c3317
Sebastián M <smunozmo@gmail.com>
2021-10-08 18:31:28 -0500

    Add classroom and rental classes.

---
+++ rental.rb
@@ -0,0 +1,12 @@
+class Rental
+    def initialize(date, book, person)
+      @date = date
+      @book = book
+      book.rentals << self
+      @person = person
+      person.rentals << self
+    end
+  
+    attr_reader :book
+    attr_accessor :date
+end
