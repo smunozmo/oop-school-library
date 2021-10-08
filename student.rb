b81f548788ad61aac44608ffa9cde5df36c4e4be
Sebastián M <smunozmo@gmail.com>
2021-10-08 18:52:30 -0500

    Update student class.

--- student.rb
+++ student.rb
@@ -1,10 +1,17 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(classroom)
-    super(age, name, parent_permission: true)
-    @classroom = classroom
-  end
+    def initialize(age, name, classroom: nil)
+        super(age, name, parent_permission: true)
+        @classroom = classroom
+    end
+
+    attr_reader :classroom
+
+    def classroom=(classroom)
+      @classroom = classroom
+      classroom.students.push(self) unless classroom.students.include?(self)
+    end  
 
   def play_hooky
     "¯\(ツ)/¯"
