49d21239405b4169e2cc4f90cd4ad91731ffdcfc
Sebastián M <smunozmo@gmail.com>
2021-10-12 16:22:04 -0500

    Add save data method.

--- app.rb
+++ app.rb
@@ -5,9 +5,13 @@ require_relative './book'
 require_relative './rental'
 require_relative './lists'
 require_relative './creators'
+require 'json'
 
 class App
   def initialize
+    file_data = File.read("test-data.json")
+    file_parse = JSON.parse(file_data)
+    p file_parse
     @people = []
     @rentals = []
     @books = []
@@ -39,9 +43,21 @@ class App
     @lists.rental(@people)
   end
 
+  def save_data
+    books_object = {}
+    @books.each_with_index do |e, idx| 
+      books_object[idx] = {'title' => e.title, 'author' => e.author}
+    end
+    json_books = books_object.to_json
+    File.write('./test-data.json', json_books)
+    print "\nData stored suceessfully.".green
+  end
+
+
   def exit_app
+    save_data
     speed = 0.2
-    print 'Thank '.pink
+    print "\nThank ".pink
     sleep(speed)
     print 'you '.pink
     sleep(speed)
